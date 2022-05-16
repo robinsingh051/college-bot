ea2429f49520146f2e75c81cbb5b060eba17a265
Robin Singh <robinsingh051s@gmail.com>
2022-05-16 23:37:56 +0530

    init commit

--- voice.py
+++ voice.py
@@ -13,28 +13,31 @@ def speak(text):
     engine.runAndWait()
 
 def takeCommand():
+    print('listening....')
     recognizer = sr.Recognizer()
     with sr.Microphone() as source:
-        print("I am Listening...")
+        # recognizer.adjust_for_ambient_noise(source)
         audio = recognizer.listen(source)
         
         try:
             statement = recognizer.recognize_google(audio, language='en-in')
             print(f"user said : {statement}\n")
         except Exception as e:
-            speak("Pardon me, can you please repeat?")
-            return "None"
+            print("Pardon me, can you please repeat?")
+            return ""
         return statement
 
 msg=''
+speak("powering on. say hello to start conversation, and bye to end")
 while True:
     msg = takeCommand().lower()
     if(msg=='hello'):
         speak("How can I help you?")
         while msg!='bye':
             msg = takeCommand().lower()
-            r=requests.post('http://localhost:5002/webhooks/rest/webhook',json={"message":msg})
-            print('Bot says, ',end=' ')
-            for i in r.json():
-                res=i['text']
-                speak(res)
+            if msg!='':
+                r=requests.post('http://localhost:5002/webhooks/rest/webhook',json={"message":msg})
+                print('Bot says, ',end=' ')
+                for i in r.json():
+                    res=i['text']
+                    speak(res)
